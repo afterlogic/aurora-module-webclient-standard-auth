e6b0da6ab1983bf990ee958c274c5822d2ed9e88
nadine-shi <nadine@afterlogic.com>
2016-08-08 13:38:52 +0300

    Create module StandardAuthWebclient with admin and user settings tabs

---
+++ js/manager.js
@@ -0,0 +1,51 @@
+'use strict';
+
+
+module.exports = function (oAppData, iUserRole, bPublic) {
+	var
+		TextUtils = require('modules/CoreClient/js/utils/Text.js'),
+		
+		Settings = require('modules/%ModuleName%/js/Settings.js'),
+		
+		bAdminUser = iUserRole === Enums.UserRole.SuperAdmin,
+		bPowerUser = iUserRole === Enums.UserRole.PowerUser
+	;
+	
+	if (bAdminUser)
+	{
+		return {
+			/**
+			 * Runs after app initializing. Adds standard auth tab to admin panel.
+			 * 
+			 * @param {Object} ModulesManager Modules manager object.
+			 */
+			start: function (ModulesManager) {
+				ModulesManager.run('AdminPanelClient', 'registerAdminPanelTab', [
+					function () { return require('modules/%ModuleName%/js/views/AccountsSettingsView.js'); },
+					Settings.HashModuleName + '-accounts',
+					TextUtils.i18n('%MODULENAME%/LABEL_BASIC_ACCOUNTS_TAB')
+				]);
+			}
+		};
+	}
+	
+	if (bPowerUser)
+	{
+		return {
+			/**
+			 * Runs after app initializing. Adds standard auth tab to user settings.
+			 * 
+			 * @param {Object} ModulesManager Modules manager object.
+			 */
+			start: function (ModulesManager) {
+				ModulesManager.run('SettingsClient', 'registerSettingsTab', [
+					function () { return require('modules/%ModuleName%/js/views/AccountsSettingsView.js'); },
+					Settings.HashModuleName + '-accounts',
+					TextUtils.i18n('%MODULENAME%/LABEL_BASIC_ACCOUNTS_TAB')
+				]);
+			}
+		};
+	}
+	
+	return null;
+};
