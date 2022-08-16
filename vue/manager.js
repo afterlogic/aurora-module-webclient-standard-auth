278ea43b3fe52461709e270334cca43921e045b9
sash04ek <a.ovcharov@afterlogic.com>
2022-08-16 16:46:21 +0300

    Rewrite tenant adminpanel on VueJS

--- vue/manager.js
+++ vue/manager.js
@@ -1,22 +1,28 @@
 import DbAdminSettingsPerUser from './components/DbAdminSettingsPerUser'
 
+import store from 'src/store'
+
 export default {
   moduleName: 'StandardAuthWebclient',
 
   requiredModules: [],
 
   getAdminUserTabs () {
-    return [
-      {
-        tabName: 'db-auth',
-        tabTitle: 'STANDARDAUTHWEBCLIENT.ADMIN_PANEL_TAB_LABEL',
-        tabRouteChildren: [
-          { path: 'id/:id/db-auth', component: DbAdminSettingsPerUser },
-          { path: 'search/:search/id/:id/db-auth', component: DbAdminSettingsPerUser },
-          { path: 'page/:page/id/:id/db-auth', component: DbAdminSettingsPerUser },
-          { path: 'search/:search/page/:page/id/:id/db-auth', component: DbAdminSettingsPerUser },
-        ],
-      }
-    ]
+    const isUserSuperAdmin = store.getters['user/isUserSuperAdmin']
+    if (isUserSuperAdmin) {
+      return [
+        {
+          tabName: 'db-auth',
+          tabTitle: 'STANDARDAUTHWEBCLIENT.ADMIN_PANEL_TAB_LABEL',
+          tabRouteChildren: [
+            { path: 'id/:id/db-auth', component: DbAdminSettingsPerUser },
+            { path: 'search/:search/id/:id/db-auth', component: DbAdminSettingsPerUser },
+            { path: 'page/:page/id/:id/db-auth', component: DbAdminSettingsPerUser },
+            { path: 'search/:search/page/:page/id/:id/db-auth', component: DbAdminSettingsPerUser },
+          ],
+        }
+      ]
+    }
+    return []
   },
 }
